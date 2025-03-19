e8a2f91c1cb4f13e130de7d9736f66d871c0783c
wnddjgld <wnddjgld@gmail.com>
2025-03-19 09:09:18 +0900

    연습

---
+++ src/test/java/kr/ac/kopo/wnddjgld/bookmarket/BookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.wnddjgld.bookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
