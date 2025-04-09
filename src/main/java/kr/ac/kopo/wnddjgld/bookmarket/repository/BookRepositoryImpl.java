2999e392f0f1ba039a7c390833b908b44a5c8f72
wnddjgld <wnddjgld@gmail.com>
2025-04-09 11:45:04 +0900

    BookMarket에 5장 상세정보 표시 구현 중 Repository 계층까지 됨

--- src/main/java/kr/ac/kopo/wnddjgld/bookmarket/repository/BookRepositoryImpl.java
+++ src/main/java/kr/ac/kopo/wnddjgld/bookmarket/repository/BookRepositoryImpl.java
@@ -62,4 +62,20 @@ public class BookRepositoryImpl implements BookRepository {
     public List<Book> getAllBookList() {
         return listOfBooks;
     }
+
+    @Override
+    public Book getBookById(String bookId) {
+        Book bookInfo = null;
+        for (Book book : listOfBooks) {
+            if (book != null && book.getBookId()!= null && book.getBookId().equals(bookId)) {
+                bookInfo = book;
+                break;
+            }
+        }
+        if (bookInfo == null) {
+            throw new IllegalArgumentException("도서번호가 " + bookId + "인 해당 도서를 찾을 수 없습니다.");
+        }
+
+        return null;
+    }
 }
